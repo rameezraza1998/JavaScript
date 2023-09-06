84b4f1ee90767122439dc3c67a3b7fc60ddc61af
rameezraza1998 <ramisraza95@gmail.com>
2023-09-06 22:54:42 +0500

    javascript class 02

---
+++ 2/app.js
@@ -0,0 +1,39 @@
+//let data ="5";
+//let data2 =5;
+
+// let data3= data+data2; // concatenation
+
+// alert(data+data2); if dono no hongay 2 addition hogi means same datatype hogi to addition otherwise concatetanion hogi
+    // num + num addition
+    // string + num concatetanion
+    // num + string concatetanion 
+    // string + string concatetanion
+
+// console.log(data3); 
+
+// javaScript is case sensitive language
+// learn more functions on it......
+
+// let d="Hello ";
+// let d2=d.includes("H"); // idhr d.includes method bata rha hai kay H hai string ma isiliyeh output bhi "true" araha hai
+// console.log(d2);
+// alert(d2);
+
+// let d="5";
+// let d2=5;
+//console.log(Number(d) + d2); // + or parseInt or number likhlo string k pehla variable k sath toh wo addition kr kay dedega
+// console.log(parseInt(d) + d2);
+// console.log(+d + d2);
+
+// MATH OERATROR  = + - * % /
+
+// let d= "10";
+// let d2="3";
+
+// console.log(d%d2); // remainder
+
+
+
+
+
+
