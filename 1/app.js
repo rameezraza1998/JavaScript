b958de15223d5398437573f127bb2b0fab6bb451
rameezraza1998 <ramisraza95@gmail.com>
2023-09-06 17:42:21 +0500

    js class 1

---
+++ 1/app.js
@@ -0,0 +1,14 @@
+alert("pls load your windows");
+let FirstName = "Rameez";
+let Last_name ="Raza"; // legal variable
+let a = 12; // legal variable
+let GoodDay = true; // legal variable
+alert(FirstName);
+alert(a);
+alert(GoodDay);
+let $FirstName="BAA"; // not a good practice
+
+let _FirstName="Abdullah"; // not a good practice
+
+let BadDay="false"; // illegal variale 
+
