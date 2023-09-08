3d02809d4e0dbdb960ce7d97d074c787542174b6
rameezraza1998 <ramisraza95@gmail.com>
2023-09-08 20:45:14 +0500

    js class 03

---
+++ 3/apps.js
@@ -0,0 +1,20 @@
+// // // // // let FavNum = prompt("Enter your favourite number !");
+
+// // // // // console.log(Number(FavNum) + 5);
+
+
+
+// // // // // comparasion operators
+// // // // 1 ==
+// // // // 2 != or !=
+// // // // 3 >=
+// // // // 4 <=
+// // // // 5 > /<
+
+// // // console.log(5 === "5"); to compare in JS always use triple equals to type check krna k liyeh 
+// // like string or number ko comapare kar kay batadyega
+
+// console.log(5 !==  6);
+
+
+// // // // //Multiple conditions through if and else
