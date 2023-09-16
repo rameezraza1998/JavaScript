f7f06259dbf68ad240ed3f1c430d403b01c5ab14
rameezraza1998 <ramisraza95@gmail.com>
2023-09-16 11:16:02 +0500

    class 4

--- 4/apps.js
+++ 4/apps.js
@@ -1,14 +1,14 @@
-//                                                       FUNCTIONS
+//                                                     custom  FUNCTIONS
 
 
 // function greet() {
-//     console.log("Hello Noor You are damnn sexy u should know this that your bf is dying for you");
+//     console.log("Hello bay You are damnn sexy u should know this");
     
 // }
 
 
 // function sexy() {
-//     console.log("Hello noor You are damnn sexy ");
+//     console.log("Hello baby You are damnn sexy ");
     
 // }
 
@@ -16,9 +16,70 @@
 // sexy();
 
 
-function name(Name){ // parameter
-    console.log("hello " + Name);
-}
+// function name(Name){ // parameter
+//     console.log("hello " + Name);
+// }
 
-name("Ramis"); // argument
-name("Noor");
+// name("Ramis"); // argument
+// name("Noor");
+
+
+// function employee(number){ // parameter
+//     return "  file no " + number;
+// }
+
+// let data =employee(5); 
+// console.log(data);
+
+
+// function add (n1,n2)
+// {
+//     return n1+n2;
+// }
+
+// let sum = add(1,3);
+// console.log(sum);
+
+
+
+// function add (n1,n2)
+// {
+//     let num1 = +(n1);
+//     let num2 = +(n2);
+
+//     return num1+num2;
+// }
+
+
+
+// let sum = add("1","3");
+// console.log(sum);
+
+// Recursion
+
+
+// function write(data ,times) {
+
+//    console.log(data);
+//     if (times > 0) {
+
+//         write(data ,times -1)
+
+//     }
+// }
+// write("Noor is beautiful ", 3);
+
+
+
+function factorial(data) {
+
+    return data;
+//      if (data >=1) {
+ 
+//         return data*factorial( data - 1);
+ 
+//      }
+//  }
+
+// let result = factorial(5);
+console.log(factorial(5));
