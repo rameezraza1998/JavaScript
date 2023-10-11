5fbe1eba9643f30e142705c3078b48e6be3de6c9
rameezraza1998 <ramisraza95@gmail.com>
2023-10-11 11:56:15 +0500

    fort class 1

---
+++ the fort/1/apps.js
@@ -0,0 +1,22 @@
+let Name="Ramis";
+console.log(Name);
+Name ="toori";
+console.log(Name);
+
+let address
+address = "55/7 ";
+console.log(address);
+address ="c/22 ";
+console.log(address);
+
+const a=5;
+console.log(a)
+
+var c=4;
+console.log(c);
+c=3;
+console.log(c);
+
+console.log(address + Name);
+console.log("=> "+(c + a)+ " addition");
+console.log("=> "+ c + a+ " concatenate");
