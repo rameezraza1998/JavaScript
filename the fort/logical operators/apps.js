ffba4f63a3c68b51b39b4c06285266d2d713d877
rameezraza1998 <ramisraza95@gmail.com>
2023-10-23 13:02:43 +0500

    operators

---
+++ the fort/logical operators/apps.js
@@ -0,0 +1,35 @@
+
+// console.log("squaring ==> "+ 10 ** 3);
+
+// let n1 = 10;
+// let n2 = 20;
+// console.log(n1);
+// console.log(n2);
+
+//  n1= n1 + n2;
+//  console.log(n1);
+
+// // ASSIGNMENT
+
+//  n1 %= 2;
+//  console.log(n1);
+//  n1 /= 2;
+//  console.log(n1);
+//  n1 *= 2;
+//  console.log(n1);
+//  n1 -= 2;
+//  console.log(n1);
+//  n1 += 2;
+//  console.log(n1);
+
+ // COMPRASSION OPERATOR
+
+ let num1 =10;
+ let num2 ='20';
+
+//  let comparision = num1 === num2;
+//  console.log(typeof (comparision), comparision);
+
+// let compareVal = num1 > num2 && num1 <= num2 && num1 == num2;
+// let compareVal =  true;
+// console.log(typeof(compareVal) , compareVal);
