96030a9b2f033579f25c5342dafbce92833ee455
rameezraza1998 <ramisraza95@gmail.com>
2023-10-28 19:29:38 +0500

    practice

---
+++ 5/app.js
@@ -0,0 +1,41 @@
+
+
+// let salary = 2000000;
+// let car = false;
+
+// if(car === "Crusier" && salary > 90000)
+// {
+//     console.log("you can marry my daughter");
+//     console.log("there is no restriction in marrying my daughter");
+
+// }
+
+// else if(car === "civic" || salary >=100000)
+// {
+//     console.log("we need timr to think");
+//     console.log("maybe u cannt make it");
+// }
+
+// else{
+//     console.log("ishtikra ma NAA aye hai");
+// }
+
+
+
+ const bankBalance = 19;
+ const car = 'fx';
+ const apnaHome = true;
+
+ if(bankBalance > 500000 && car === 'fortuner' && apnaHome === true){
+     console.log('rishta mubarak hoo');
+     console.log('Mithai leta hua ana');
+     console.log('dadi bhi saath ayengi');
+ } 
+ else if(bankBalance > 150000 && car === 'civic' && apnaHome === true){
+    console.log('abba maan gaye hain');} 
+    else if(bankBalance > 500000 || car ==='mehran' && apnaHome === false){
+     console.log('istekhara ma naa aya ha');
+ 
+ }else{
+     console.log('istekhara ma naa aya ha , abba nahi maan rha , ammi nas kaat laingi..');
+ }
