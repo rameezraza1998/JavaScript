b56fe54fcf569c55f8cb074afbb54ed85651630c
rameezraza1998 <ramisraza95@gmail.com>
2023-11-01 06:22:24 +0500

    functions practice

---
+++ 6/app.js
@@ -0,0 +1,34 @@
+
+
+// let n1 = +prompt("enter no 1:");
+// let n2 = +prompt("enter no 2:");
+
+// let sum = n1 + n2;
+// alert(sum);
+
+// console.log(typeof(n2));
+
+// let s = "ali";
+// let a = +s;             // hamesha variable k sath hi + lagega to convert type of data to number.
+
+// console.log(typeof(a));
+
+let a = 15;
+let b = 10;
+let c = 15;
+let d = 20; 
+let sum;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+if ( (a<b) || (c>b) && (c<d) )
+{
+    sum = a+b+c+d;
+    console.log(sum);
+}
+else {
+    console.log(d);
+}
---
+++ prac classs of functions/app.js
@@ -0,0 +1,223 @@
+// console.log('hello world');
+
+// let 
+// var
+// const
+
+
+// var user = 'abdullah';
+// var user = 'hamza'; // redecleration
+// user = 'ahmed' // reaasign
+
+
+
+// let user = 'abdullah'; declaration
+// user = 'rameez'; //reassign
+// let user = 'ahmed' // redeclaration and reassign
+
+
+
+//let users;
+//var userss;
+// const user; this is wrong because here we have to assin values also
+
+
+
+
+
+
+
+
+// const comparision = 3 !== "3";
+// console.log(comparision);
+
+// ==
+// ===
+// != check without data type
+// !== check with data type
+
+
+
+
+
+
+
+
+
+
+
+// false or true = true
+// false or false = false
+// true or true =true
+// false and false = false
+// false and  true =false
+// true and true = true
+
+
+
+
+
+
+
+
+
+
+// if (3 > 3 || 3 < 3 && 3 >= 5 || 3 <= 50){
+//     console.log('if chala ga');
+// }else{
+//     console.log('else chala ga');
+// }
+
+
+
+
+
+
+
+
+
+
+
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+
+
+
+// function repeatConsole() {
+//     console.log('hello world');
+//     console.log('hello world');
+//     console.log('hello world');
+// }
+// repeatConsole()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let names = prompt('enter your name');
+// let age = +prompt('enter your age');
+
+
+// function greetUser(userName , age) {
+//     if (isNaN(+names) && typeof (age) === 'number') {
+//         console.log('hello ' + userName + ' and my age is ' + age);
+//     } else {
+//         console.log('bhaijaaan sahi value do');
+//     }
+// }
+
+
+// greetUser(names , age)
+
+
+// let Username = prompt("enter name of user:");
+// let age = +prompt("enter age of user:");
+
+// function ask(name, age)
+// {
+//     if(isNaN(name) && typeof(age)==="number")
+//     {
+//         alert("your name is: "+ name + " and age is: " +age);
+//     }
+//     else{
+//         alert("write correct information please");
+//     }
+// }
+// ask(Username, age);
+
+
+//         my work
+
+
+
+// function sum(num1, num2) {
+//     console.log(num1 + num2);
+//     return num1 + num2
+// }
+
+// sum(20 , 30);
+// console.log(sum(20, 30));
+// console.log(sum(20 , 30));
+
+
+// here we are returning values 
+
+// function multiply(n1,n2)
+// {
+//    // mul = n1*n2;
+//    //  return mul;
+//    return n1*n2;
+// }
+
+// console.log(multiply(5,5));
+
+// console.log(mul);
+
+
+
+
+
+
+
+
+// function gharKaNoker(samaan1 , samaan2){
+//     // console.log(samaan1 , samaan2);
+//     let returnVal =  samaan1 +" "+ samaan2
+//     return returnVal
+// }
+// gharKaNoker('doodh' , 'dhaniya');
+// // console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function ramooKaka (dish1 , dish2 , desert){
+//     return "yelo bhaijan appka khana " + dish1 + " or " + dish2 + " or meetha ma " + desert + " haa"
+// }
+
+
+// console.log(ramooKaka('tindaa' , 'roti' , 'gurrr'));
