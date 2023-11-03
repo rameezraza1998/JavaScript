91c500275b05d6700b82c80aac58eb8a70b1963e
rameezraza1998 <ramisraza95@gmail.com>
2023-11-03 21:07:32 +0500

    practice return function

---
+++ 7/app.js
@@ -0,0 +1,23 @@
+
+// function grocery(item1,item2,item3)
+// {
+//     console.log("these are the items we bought: "+item1+ " "+item2+ " "+item3);
+
+// }
+
+// grocery("olper","chilli-powder","pulses");
+
+
+
+
+function Clinic(doc1,doc2,doc3,age)
+{
+    name1= doc1;
+    name2= doc2;
+    name3= doc3;
+    age1=age;
+    return (name1,name2,name3,age1);
+}
+
+Clinic("noor","ramis","sadaf",34);
+console.log("the doctors are: "+name1+ " "+name2+" "+name3+" "+age1);
