e426e391eb98d64c14cf3e6371aaad6cae058cf3
rameezraza1998 <ramisraza95@gmail.com>
2023-12-23 11:46:39 +0500

    date and time switch

--- color changing div by js/app.js
+++ color changing div by js/app.js
@@ -3,18 +3,6 @@ const body = document.querySelector('body')
 const div = document.querySelector('div')
 const color = ['red' , 'green' , 'blue' , 'orange' , 'purple' , 'pink' , 'yellow' , 'aqua']
 
-// for(let i = 0; i<color.length; i++ )
-// {
-// div.innerHTML +=  `<button  onclick="changeColor(${i})">${color[i]}</button>`
-
-
-// }
-// function changeColor(i){
-//     console.log(color[i]);
-//     body.style.backgroundColor = color[i];
-// }
-
-
 for (let i = 0; i<color.length; i++)
 {
     div.innerHTML += `<button style="margin: 10px;"  onclick="change(${i})">${color[i]}</button>`
---
+++ date and time/app.js
@@ -0,0 +1,41 @@
+let date = new Date();
+
+// console.log(date);
+
+// console.log(date.toString());
+// console.log(date.toLocaleDateString());
+// console.log(date.getDay());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getFullYear());
+// console.log(date.getTime());
+
+
+
+// const days = ['sunday' , 'monday' , 'tuesday' , 'wednesday' , 'thursday' , 'friday' , 'saturday'];
+
+// console.log(days[date.getDay()]);
+
+
+// const Date1 = ['1' , '2' , '3' , '4' , '5' , '6' , '7', '8', '9', '10','11' , '12' , '13' , '14' , '15' , '16' , '17', '18', '19', '20',
+// '21' , '22' , '23' , '24' , '25' , '26' , '27', '28', '29', '30'];
+
+// console.log(Date1[date.getDate()]);
+
+
+
+
+
+//How many milliseconds ashad wasted since he born.
+
+// const currentDate = new Date();
+// const ashadBirthday = new Date('January 20 , 2004');
+// // const milliseconds = currentDate.getTime();
+// // const ashadMilliseconds = ashadBirthday.getTime();
+// // const remainingMilliseconds = milliseconds - ashadMilliseconds;
+
+// // console.log('wasted years by ashad', Math.ceil(remainingMilliseconds/1000/60/60/24));
+
+// console.log(currentDate.getTime());
+
+// console.log(ashadBirthday.getTime());
