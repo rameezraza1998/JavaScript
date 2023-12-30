92373c135c173d8d7ede16a3dd223becc40e38f4
rameezraza1998 <ramisraza95@gmail.com>
2023-12-30 11:12:00 +0500

    reduce and filter completed

--- array loop part reduce filter/app.js
+++ array loop part reduce filter/app.js
@@ -90,3 +90,40 @@ const Mynums =[1,2,3,4,5,6,7,8,9,10]
 // REDUCE
 
 const array1=[1,2,3,4]
+
+// const total = array1.reduce( function (acc,currval) {
+//     console.log(acc,'+',currval);
+//     return acc + currval
+// }, 0)
+
+
+// const total = array1.reduce(  (acc,currval) => acc+currval, 0)
+
+
+// console.log(total);
+
+
+// ADD TO CART METHOD
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
